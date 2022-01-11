709c8996e73886964eba6d833924e55fff0ca9b8
QemerS <tu201806169@code.edu.az>
2022-01-11 07:03:25 +0400

    hazir deyil

---
+++ WebApp/WebApp/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ID { get; set; }
+        [MaxLength(20), Required(ErrorMessage = "Title is not valid")]
+        public string Title { get; set; }
+        [MaxLength(250)]
+        public string Image { get; set; }
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
+        [Required(ErrorMessage = "Price is not valid")]
+        public decimal Price { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ WebApp/WebApp/Models/Setting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class Setting
+    {
+        public int ID { get; set; }
+        [MaxLength(255)]
+        public string Logo { get; set; }
+        [MaxLength(255)]
+        public string UpBannerTitle { get; set; }
+        [MaxLength(255)]
+        public string LowerBannerTitle { get; set; }
+
+    }
+}
